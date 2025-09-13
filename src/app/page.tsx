5a3f4389e534f46ba2b4fbfa9e3bfc62634ffad7
faseeh ali <34987986+FaseehFury@users.noreply.github.com>
2025-09-13 05:58:49 +0000

    Add featured image on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image"
+
+
+
 const Homepage = () => {
+  //image parent should not be static it should be relative if we need to use fill for image
+  //for aspect we say height will be 3 times smaller than width
   return (
-    <div className='bg-red-200'>Homepage</div>
+    <div className=''>
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src="/featured.png" alt="Featured Product" fill/>
+      </div>
+    </div>
   )
 }
 
