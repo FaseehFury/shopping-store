2eab5e8ef7a91f299f9d800230bce9e5eb704e81
FaseehFury <faseehali506@gmail.com>
2025-09-16 13:17:45 +0500

    working on cart page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
-
-
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
 
 const Homepage = () => {
   //image parent should not be static it should be relative if we need to use fill for image
   //for aspect we say height will be 3 times smaller than width
   return (
-    <div className=''>
+    <div className="">
       <div className="relative aspect-[3/1] mb-12">
-        <Image src="/featured.png" alt="Featured Product" fill/>
+        <Image src="/featured.png" alt="Featured Product" fill />
       </div>
+      <ProductList />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
